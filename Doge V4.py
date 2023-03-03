99a50f5c801f5067ed3ef5b6083e6e411f84913b
ssit11 <104804435+ssit11@users.noreply.github.com>
2023-03-04 00:07:15 +1000

    Update and rename Doge V3.py to Doge V4.py

--- Doge V4.py
+++ Doge V4.py
@@ -4,36 +4,39 @@ import random
 import datetime
 import tkinter.scrolledtext as scrolledtext
 
+
 class DodgeGUI:
     def __init__(self, master):
         self.master = master
         master.title("Dodge")
+        master.configure(bg="#1E1E1E")
 
         # Create the text input box
-        self.text_box = tk.scrolledtext.ScrolledText(master, height=10, width=50, wrap="word")
+        self.text_box = tk.scrolledtext.ScrolledText(master, height=10, width=50, wrap="word", bg="#2E2E2E", fg="#F8F8F8", insertbackground="#F8F8F8", highlightbackground="#2E2E2E", highlightcolor="#F8F8F8", font=("Helvetica", 12))
         self.text_box.pack(fill="both", expand=True)
 
         # Create the "Check" button
-        self.check_button = tk.Button(master, text="Check", command=self.check_text)
-        self.check_button.pack()
+        self.check_button = tk.Button(master, text="Check", command=self.check_text, bg="#F8F8F8", fg="#1E1E1E", activebackground="#1E1E1E", activeforeground="#F8F8F8", font=("Helvetica", 12, "bold"))
+        self.check_button.pack(pady=10)
 
         # Create the output text box
-        self.output_text_box = tk.scrolledtext.ScrolledText(master, height=10, width=50, wrap="word")
+        self.output_text_box = tk.scrolledtext.ScrolledText(master, height=10, width=50, wrap="word", bg="#2E2E2E", fg="#F8F8F8", insertbackground="#F8F8F8", highlightbackground="#2E2E2E", highlightcolor="#F8F8F8", font=("Helvetica", 12))
         self.output_text_box.pack(fill="both", expand=True)
 
         # Create the "Copy" button
-        self.copy_button = tk.Button(master, text="Copy", command=self.copy_text)
-        self.copy_button.pack()
+        self.copy_button = tk.Button(master, text="Copy", command=self.copy_text, bg="#F8F8F8", fg="#1E1E1E", activebackground="#1E1E1E", activeforeground="#F8F8F8", font=("Helvetica", 12, "bold"))
+        self.copy_button.pack(pady=10)
 
         # Create the list of ChatGPT detectors
         self.detectors = {
-            "GPTZero works with Doge v1.0 and up": lambda text: random.choice([True, False]),  # Replace with your actual detection logic
-            "contentatscale.ai works with Doge v3.0": lambda text: random.choice([True, False])   # Replace with your actual detection logic
+            "GPTZero": lambda text: random.choice([True, False]),  # Replace with your actual detection logic
+            "contentatscale.ai": lambda text: random.choice([True, False])   # Replace with your actual detection logic
         }
 
         # Create the detector list label
-        self.detector_list_label = tk.Label(master, text=self.get_detector_list_text())
-        self.detector_list_label.pack()
+        self.detector_list_label = tk.Label(master, text=self.get_detector_list_text(), bg="#1E1E1E", fg="#F8F8F8", font=("Helvetica", 10))
+        self.detector_list_label.pack(pady=5)
+
 
     def check_text(self):
         # Get the text from the input box
@@ -47,7 +50,7 @@ class DodgeGUI:
             fake_text = self.generate_fake_text(text)
             self.output_text_box.config(state="normal")
             self.output_text_box.delete("1.0", "end")
-            self.output_text_box.insert("end", f"The input text was written by ChatGPT with a probability of 80%. \n\nHere's a modified version of the text that looks more like it was written by a human:{'-'*50}\n\n{fake_text}")
+            self.output_text_box.insert("end", f"The input text was written by ChatGPT with a probability of 80%. \n\nHere's a modified version of the text that looks more like it was written by a human:\n{'-'*90}\n{fake_text}")
             self.output_text_box.config(state="disabled")
         else:
             self.output_text_box.config(state="normal")
@@ -55,6 +58,8 @@ class DodgeGUI:
             self.output_text_box.insert("end", "The input text was not written by ChatGPT.")
             self.output_text_box.config(state="disabled")
 
+        # Update the detector list label
+        self.detector_list_label.config(text=self.get_detector_list_text())
     def generate_fake_text(self, text):
         # Replace this with your actual text modification logic
         return text.replace(".", ". ").replace(",", ", ")
@@ -67,7 +72,7 @@ class DodgeGUI:
 
     def get_detector_list_text(self):
         # Generate a list of detectors and their status
-        detector_list = ["ChatGPT detectors:"]
+        detector_list = ["detectors:"]
         for name, detector in self.detectors.items():
             status = "working" if detector("test text") is not None else "not working"
             detector_list.append(f"{name}: {status}")
@@ -76,7 +81,7 @@ class DodgeGUI:
         detector_list_text = "\n".join(detector_list)
 
         # Add the date and time the detector list was last updated
-        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        current_time = "03/03/2023"
         detector_list_text += f"\n\nLast updated: {current_time}"
 
         return detector_list_text
@@ -84,4 +89,3 @@ class DodgeGUI:
 root = tk.Tk()
 my_gui = DodgeGUI(root)
 root.mainloop()
-
